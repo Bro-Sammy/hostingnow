a65851035ccce244092c1bcb305ac26195433bbf
Samuel Amoah <skwaku6@gmail.com>
2020-04-23 17:17:11 +0000

    Hosting My website

---
+++ app.js
@@ -0,0 +1,10 @@
+function hello(){
+    alert(`
+        Hello! Welcome To The Coding World
+        ✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼✍🏼
+
+        You are going really enjoy this Class
+
+        We promise to let all the cats 🐈 out of the bag
+    `)
+}
